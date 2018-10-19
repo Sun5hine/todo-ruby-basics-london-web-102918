837626832f87aeb0313cefb03347bd687ab9fc83
sun5hine <af5kh@yahoo.com>
2018-10-19 15:21:23 +0000

    automatically backed up by learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -15,11 +15,11 @@ def greeting(name1, name2)
   return name2
 end
 
-def return_a_value
-return "bob"
+def return_a_value(name = "bob")
+return name
 end
 
-def last_evaluated_value(name)
+def last_evaluated_value(name = "nice")
 return name
 end
 
